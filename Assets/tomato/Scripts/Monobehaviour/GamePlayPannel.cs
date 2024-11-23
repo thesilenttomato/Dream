0be88296f783aef758dd8efeaa7e84a864369dd6
thesilenttomato <yzxc5418@163.com>
2024-11-23 15:44:41 +0800

    完成了血条的制作

--- Assets/tomato/Scripts/Monobehaviour/GamePlayPannel.cs
+++ Assets/tomato/Scripts/Monobehaviour/GamePlayPannel.cs
@@ -1,14 +1,36 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class GamePlayPannel : MonoBehaviour
 {
    private VisualElement root;
+   private VisualElement hpContainer;
+   public VisualTreeAsset zzzTemple;
+   public IntVarible hpVarible;
+   public int maxHp { get => hpVarible.maxVaule; }
+   public int currentHp { get => hpVarible.currentVaule; set => hpVarible.SetValue(value); }
 
    private void OnEnable()
    {
       root = GetComponent<UIDocument>().rootVisualElement;
-      
+      hpContainer = root.Q<VisualElement>("HpContainer");
+
+      InitHp();
+   }
+
+   private void InitHp()
+   {
+      // 清空 hpContainer 现有的子元素（防止重复生成）
+      //hpContainer.Clear();
+      for (int i = 0; i < currentHp; i++)
+      {
+        
+         // 创建新的 zzz 元素（克隆原始模板 zzz）
+         VisualElement temple = zzzTemple.Instantiate();
+         var  zzz  = temple.Q<VisualElement>("zzz");
+         hpContainer.Add(zzz);
+      }
    }
 }
